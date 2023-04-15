91580b36809bcf19c0f2270497ca62b244ac56c6
feelu10 <carljamesberdos101998@gmail.com>
2023-04-16 00:13:35 +0800

    updating git ignore

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -79,7 +79,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header"><h1>Login</h1></div>
+                    <div class="card-header"><h1>Login Form</h1></div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('login') }}" id="login-form">
